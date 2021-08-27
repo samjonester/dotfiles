706cbbc9eeeb28d693359b74b701b831ce9df3d1
Sam Schmidt <sam.schmidt@shopify.com>
2021-08-27 17:03:54 -0400

    Add git configs

--- install.sh
+++ install.sh
@@ -21,7 +21,6 @@ fi
 # Load the antigen plugin/theme manager
 source ~/antigen/antigen.zsh
 
-
 if [ $SPIN ]; then
   # Install Ripgrep for better code searching: `rg <string>` to search. Obeys .gitignore
   sudo apt-get install -y ripgrep
@@ -29,13 +28,17 @@ if [ $SPIN ]; then
   # Set system generated .gitconfig to .gitconfig.local. We'll pull it in later as part
   # of our custom gitconfig. The generated gitconfig already has the right user and email,
   # since Spin knows that from login.
-  mv ~/.gitconfig ~/.gitconfig.local
+  mv -n ~/.gitconfig ~/.gitconfig.local
 fi
+
+# Symlink core configs
+
+# Link in the custom gitconfig. This has to happen after we rename to .gitconfig.local, otherwise we clobber the
+# spin generated user config.
+ln -vsfn ~/dotfiles/core/configs/.gitconfig ~/.gitconfig
+ln -vsfn ~/dotfiles/core/configs/.gitignore ~/.gitignore
 
 # Symlink this repo's .zshrc to ~/.zshrc. Using a symlink ensures that when the repo is
 # updated, the terminal will pick up the new version on reload without having to run
 # install again. This will overwrite any existing .zshrc.
 ln -vsfn ~/dotfiles/.zshrc ~/.zshrc
-
-# Run customizations that only need to happen on repo install
-test -f ~/dotfiles/personal/install.sh && source ~/dotfiles/personal/install.sh
