9af34512a0186d6a123e31943b753bffba20e241
Sam Schmidt <sam.schmidt@shopify.com>
2021-08-27 22:12:28 -0400

    Add missing use oh-my-zsh

--- core/default_bundles.zsh
+++ core/default_bundles.zsh
@@ -3,7 +3,7 @@
 source ~/antigen/antigen.zsh
 
 # Install oh-my-zsh with paths set properly for Antigen bundles
-# antigen use oh-my-zsh
+antigen use oh-my-zsh
 
 # Install autocompletion for bundler
 # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/bundler
