6a0806add32b6b0a52400e7e0c06f15514cd7f12
Sam Schmidt <sam.schmidt@shopify.com>
2021-08-27 20:54:38 -0400

    comments

--- personal/macos.zsh
+++ personal/macos.zsh
@@ -1,1 +1,2 @@
 # Custom configs for MacOS environments.
+# This file will only be executed if the current environment is MacOS.
--- personal/spin.zsh
+++ personal/spin.zsh
@@ -1,1 +1,2 @@
 # Custom configs for Spin environments
+# This file will only be executed on Spin environments.
