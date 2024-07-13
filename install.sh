ba6c1ca332791e1666160b90b20d0b843f7f889e
Dom DeWolfe <domdewolfe@gmail.com>
2024-07-13 14:49:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ fi
 
 if [ $SPIN ]; then
   # Install Ripgrep for better code searching: `rg <string>` to search. Obeys .gitignore
-  sudo apt-get install -y ripgrep
+  sudo apt-get install -y ripgrep bat
 
   # Set system generated .gitconfig to .gitconfig.local. We'll pull it in later as part
   # of our custom gitconfig. The generated gitconfig already has the right user and email,
