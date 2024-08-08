3be157e8ccbeb5c4e8f20c2dcd0a7b72eee36442
Sam Jones <sam.jones@shopify.com>
2024-08-08 11:39:34 -0400

    Adds local environment variables

--- personal/environment.zsh
+++ personal/environment.zsh
@@ -1,2 +1,14 @@
 # Define custom environment variables.
 # This will overwrite any environment variables defined by `core/environment.zsh`.
+
+# Terminal Settings
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+[[ "$TERM" == "xterm" ]] && export TERM=xterm-256color
+
+
+# Editors and Browsers
+export EDITOR='vim'
+export VISUAL='vim'
+export PAGER='vim'
+export BROWSER='open'
