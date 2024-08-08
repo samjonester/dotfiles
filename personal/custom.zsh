17e26d243b578cad3c57e26c63b5e81b6504cc08
Sam Jones <sam.jones@shopify.com>
2024-08-08 13:52:23 -0400

    Adds custom functions and aliases including git bare-clone

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -2,3 +2,55 @@
 # This file is loaded after everything else, but before Antigen is applied and ~/extra.zsh sourced.
 # Put anything here that you want to exist on all your environment, and to have the highest priority
 # over any other customization.
+
+alias v=vim
+alias g=git
+alias n=npm
+
+alias fixcam='sudo killall VDCAssistant'
+alias fixwifi='sudo ifconfig en0 down && sudo ifconfig en0 up'
+
+alias ls='eza --long --all --group --header --classify --git'
+
+
+mcd() {
+    mkdir -p $1
+    cd $1
+}
+
+ps-kill() {
+  ps aux | grep "$1" | grep -v grep | awk '{ print $2 }' | xargs kill -9
+}
+
+# Examples of call:
+# git-clone-bare-for-worktrees git@github.com:name/repo.git
+# => Clones to a /repo directory
+#
+# git-clone-bare-for-worktrees git@github.com:name/repo.git my-repo
+# => Clones to a /my-repo directory
+bare-clone() {
+  url=$1
+  basename=${url##*/}
+  name=${2:-${basename%.*}}
+
+  mkdir $name
+  cd "$name"
+
+  # Moves all the administrative git files (a.k.a $GIT_DIR) under .bare directory.
+  #
+  # Plan is to create worktrees as siblings of this directory.
+  # Example targeted structure:
+  # .bare
+  # main
+  # new-awesome-feature
+  # hotfix-bug-12
+  # ...
+  git clone --bare "$url" .bare
+  echo "gitdir: ./.bare" > .git
+
+  # Explicitly sets the remote origin fetch so we can fetch remote branches
+  git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+
+  # Gets all branches from origin
+  git fetch origin
+}
