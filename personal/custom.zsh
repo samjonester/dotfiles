afcd77cc5c0d8737be69d4d50f6703b1e04623d9
Sam Jones <sam.jones@shopify.com>
2024-08-12 15:57:15 -0400

    adds fzf preview options

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -20,7 +20,9 @@ source <(fzf --zsh)
 source ~/src/github.com/junegunn/fzf-git.sh/fzf-git.sh
 export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
 export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+export FZF_ALT_C_OPTS="--preview 'eza --tree --level=2 --color=always {} | head -500'"
 
 _fzf_compgen_path() {
   fd --hidden --exclude .git . "$1"
