19802ba387033f8178698db91ae15c29140aa15e
Sam Jones <sam.jones@shopify.com>
2024-08-13 00:45:06 -0400

    Fixes window size for Aerial window

--- personal/nvim/lua/plugins/core.lua
+++ personal/nvim/lua/plugins/core.lua
@@ -32,4 +32,15 @@ return {
     },
   },
   { "folke/flash.nvim", enabled = false },
+  require("aerial").setup({
+    resize_to_content = true,
+    layout = {
+      -- These control the width of the aerial window.
+      -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      -- min_width and max_width can be a list of mixed types.
+      -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+      max_width = { 0.4, 1000 },
+      min_width = { 0.2, 300 },
+    },
+  }),
 }
