12bb1ff549c7b72b12e2754982d70e2aaf12a9da
Sam Jones <sam.jones@shopify.com>
2024-08-26 11:12:50 -0400

    Removes aerial size config attempt

--- personal/nvim/lua/plugins/core.lua
+++ personal/nvim/lua/plugins/core.lua
@@ -20,10 +20,6 @@ $$\   $$ |
 ]]
 return {
   {
-    "LazyVim/LazyVim",
-    opts = {},
-  },
-  {
     "nvimdev/dashboard-nvim",
     opts = {
       config = {
@@ -32,15 +28,4 @@ return {
     },
   },
   { "folke/flash.nvim", enabled = false },
-  require("aerial").setup({
-    resize_to_content = true,
-    layout = {
-      -- These control the width of the aerial window.
-      -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-      -- min_width and max_width can be a list of mixed types.
-      -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
-      max_width = { 0.4, 1000 },
-      min_width = { 0.2, 300 },
-    },
-  }),
 }
