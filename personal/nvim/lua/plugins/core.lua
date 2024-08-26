2dce6cd4723f864f4da0cc968e8ee5156bcc72f8
Sam Jones <sam.jones@shopify.com>
2024-08-26 14:03:10 -0400

    Adds ranger to neovim

--- personal/nvim/lua/plugins/core.lua
+++ personal/nvim/lua/plugins/core.lua
@@ -36,4 +36,16 @@ return {
       require("ufo").setup()
     end,
   },
+  {
+    "kelly-lin/ranger.nvim",
+    config = function()
+      require("ranger-nvim").setup({ replace_netrw = true })
+      vim.api.nvim_set_keymap("n", "<leader>ef", "", {
+        noremap = true,
+        callback = function()
+          require("ranger-nvim").open(true)
+        end,
+      })
+    end,
+  },
 }
