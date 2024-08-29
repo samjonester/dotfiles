2df9533c773f0bae6496544715de6729998eca41
Sam Jones <sam.jones@shopify.com>
2024-08-29 10:29:01 -0400

    Adds delta to Spin

--- personal/install.sh
+++ personal/install.sh
@@ -73,11 +73,13 @@ linux*)
     git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 
     # Git tooling
-    sudo apt-get install -y tig delta
+    sudo apt-get install -y tig
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     tar xf lazygit.tar.gz lazygit
     sudo install lazygit /usr/local/bin
+    curl -Lo /tmp/delta.deb https://github.com/dandavison/delta/releases/download/0.18.1/git-delta_0.18.1_amd64.deb
+    sudo dpkg -i /tmp/delta.deb
 
     # Search
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
