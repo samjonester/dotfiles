10eb6cd9bf9653171d2c0e9eb4345d04a913979e
Sam Jones <sam.jones@shopify.com>
2025-10-28 09:30:53 -0400

    Ignore asdf

--- personal/macos.zsh
+++ personal/macos.zsh
@@ -9,9 +9,9 @@
 eval "$(zoxide init zsh)"
 
 # ASDF Setup
-. $(brew --prefix)/opt/asdf/libexec/asdf.sh
-. /opt/homebrew/opt/asdf/libexec/asdf.sh
-. /opt/homebrew/opt/asdf/libexec/asdf.sh
+# . $(brew --prefix)/opt/asdf/libexec/asdf.sh
+# . /opt/homebrew/opt/asdf/libexec/asdf.sh
+# . /opt/homebrew/opt/asdf/libexec/asdf.sh
 
 # Google cloud cli and autocompletions
 if [ -f '/Users/sam/src/google/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/sam/src/google/google-cloud-sdk/path.zsh.inc'; fi
